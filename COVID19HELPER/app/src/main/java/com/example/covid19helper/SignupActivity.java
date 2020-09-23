38fd232ec444fa026f5a585c174e213ccf8e8318
Shinipsae <s2019s13@e-mirim.hs.kr>
2020-09-23 23:57:38 +0900

    add activity_alarm.xml

--- COVID19HELPER/app/src/main/java/com/example/covid19helper/SignupActivity.java
+++ COVID19HELPER/app/src/main/java/com/example/covid19helper/SignupActivity.java
@@ -9,10 +9,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,40 +30,5 @@ public class SignupActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
 
         signupButton = findViewById(R.id.signupButton);
-        signupButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                // EditText에 현재 입력되어있는 값을 가져옴
-                String userName = name.getText().toString();
-                String userYYMMDD = yymmdd.getText().toString();
-                String userSchool = school.getText().toString();
-                String userID = id.getText().toString();
-                String userPassword = password.getText().toString();
-
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            boolean success = jsonObject.getBoolean("success");
-                            if (success) {
-                                Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                        } catch (JSONException e)  {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                // 서버로 Volley를 이용해서 요청을 함.
-                RegisterRequest registerRequest = new RegisterRequest(userName, userYYMMDD, userSchool, userID, userPassword, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(SignupActivity.this);
-                queue.add(registerRequest);
-            }
-        });
     }
 }
